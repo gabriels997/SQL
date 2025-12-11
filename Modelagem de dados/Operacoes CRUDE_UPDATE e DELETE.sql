3978e292b6226b53dc6d7e968331334247975f3b
gabriels997 <gabriel.s997@outlook.com>
2025-12-11 07:46:14 -0300

    Operações CRUDE: UPDATE e DELETE
    Atenção no WHERE

---
+++ Modelagem de dados/Operacoes CRUDE_UPDATE e DELETE.sql
@@ -0,0 +1,22 @@
+--                           Terceira operação do CRUDE - UPDATE
+--  !!Atenção no WHERE!!
+
+
+-- Comando UPDATE:
+-- Update --
+UPDATE usuarios SET endereco = 'Nova Rua, 123' 
+WHERE email = 'joao@example.com';
+
+UPDATE usuarios
+SET id = 4 -- mais de 1 usuario estava com o ID 1, fiz a alteração
+WHERE email = "teste@teste.com";
+
+
+--                           Quarta operação do CRUDE - DELETE
+--  !!Atenção no WHERE!!
+
+
+-- Comando DELETE
+-- delete --
+DELETE FROM reservas 
+WHERE status = 'cancelada';
